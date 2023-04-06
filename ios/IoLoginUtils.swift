a841f923bb24c52c1c55811e993f01891dfcf3dd
Sabino Picariello <sabino.picariello@pagopa.it>
2023-04-06 16:08:24 +0200

    chore: initial commit

---
+++ ios/IoLoginUtils.swift
@@ -0,0 +1,8 @@
+@objc(IoLoginUtils)
+class IoLoginUtils: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}

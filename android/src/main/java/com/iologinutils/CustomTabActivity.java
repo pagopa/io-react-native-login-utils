bcb9a18faa2fd87b78110827cd591e2576dbb805
Sabino Picariello <sabino.picariello@pagopa.it>
2023-04-14 16:51:12 +0200

    Added a loading spinner in the screen after login - android

--- android/src/main/java/com/iologinutils/CustomTabActivity.java
+++ android/src/main/java/com/iologinutils/CustomTabActivity.java
@@ -1,32 +1,20 @@
 package com.iologinutils;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.transition.Slide;
-import android.util.AttributeSet;
-import android.util.Log;
+
 import android.view.Gravity;
 import android.view.View;
-import android.view.ViewTreeObserver;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 
-import androidx.activity.OnBackPressedCallback;
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.core.app.ActivityOptionsCompat;
-import androidx.core.app.SharedElementCallback;
 
 import com.facebook.react.bridge.Promise;
 
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
 
 public class CustomTabActivity extends Activity {
 
@@ -41,6 +29,7 @@ public class CustomTabActivity extends Activity {
     context = this;
     super.onCreate(savedInstanceState);
     handleExtra(getIntent());
+    overridePendingTransition(0, 0); // Disable animation
   }
 
   // If your app is in opened in background and you attempt to open an url, it
@@ -66,10 +55,19 @@ public class CustomTabActivity extends Activity {
     } catch (Exception e) {
       promise.reject("error", "Error in resolving promise");
     } finally {
-      View v = new View(customTabContext);
-      setContentView(v);
 
-      v.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
+
+      ProgressBar progressBar = new ProgressBar(customTabContext, null, android.R.attr.progressBarStyleLarge);
+      progressBar.setIndeterminate(true);
+      FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+      layoutParams.gravity = Gravity.CENTER;
+      FrameLayout frameLayout = new FrameLayout(customTabContext);
+      frameLayout.addView(progressBar, layoutParams);
+      setContentView(frameLayout);
+
+
+      overridePendingTransition(0, 0);
+      frameLayout.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
         @Override
         public void onViewAttachedToWindow(View view) {
           if(navigationEvent == 4 || navigationEvent == 2 || navigationEvent == 6){

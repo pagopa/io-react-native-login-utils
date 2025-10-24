53eb96261bbd60af958f612387582082e3cd0586
Cristiano Tofani <cristiano.tofani@pagopa.it>
2025-10-24 10:31:58 +0200

    chore: [IOPLT-1392] Bumps react-native to 0.81

--- example/ios/IoLoginUtilsExample/AppDelegate.swift
+++ example/ios/IoLoginUtilsExample/AppDelegate.swift
@@ -4,18 +4,36 @@ import React_RCTAppDelegate
 import ReactAppDependencyProvider
 
 @main
-class AppDelegate: RCTAppDelegate {
-  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    self.moduleName = "IoLoginUtilsExample"
-    self.dependencyProvider = RCTAppDependencyProvider()
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+ 
+  var reactNativeDelegate: ReactNativeDelegate?
+  var reactNativeFactory: RCTReactNativeFactory?
 
-    // You can add your custom initial props in the dictionary below.
-    // They will be passed down to the ViewController used by React Native.
-    self.initialProps = [:]
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    let delegate = ReactNativeDelegate()
+    let factory = RCTReactNativeFactory(delegate: delegate)
+        delegate.dependencyProvider = RCTAppDependencyProvider()
 
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    reactNativeDelegate = delegate
+    reactNativeFactory = factory
 
+    window = UIWindow(frame: UIScreen.main.bounds)
+
+    factory.startReactNative(
+      withModuleName: "IoLoginUtilsExample",
+      in: window,
+      launchOptions: launchOptions
+    )
+    
+    return true
+  }  
+}
+
+class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
   override func sourceURL(for bridge: RCTBridge) -> URL? {
     self.bundleURL()
   }
